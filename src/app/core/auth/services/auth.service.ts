b0f4738371d2cb68161a53bc8ef435a9b2aba096
Ahmed Anwer <104807894+realAhmedAnwer@users.noreply.github.com>
2026-03-30 03:54:19 +0200

    Update configuration

--- src/app/core/auth/services/auth.service.ts
+++ src/app/core/auth/services/auth.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { environment } from '@env/environment.development';
+import { environment } from '@env/environment';
 import { API_ENDPOINTS } from '@core/constants/api.constants';
 
 @Injectable({
@@ -9,7 +9,7 @@ import { API_ENDPOINTS } from '@core/constants/api.constants';
 })
 export class AuthService {
   private readonly _httpClient = inject(HttpClient);
-  
+
   signup(data: any): Observable<any> {
     return this._httpClient.post(`${environment.baseUrl}/${API_ENDPOINTS.users.signup}`, data);
   }
